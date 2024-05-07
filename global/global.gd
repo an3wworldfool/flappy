1765e536c502a7970d641edafd8f52e89cca2593
an3wworldfool <ing.germanspdf@gmail.com>
2024-05-06 18:32:36 -0700

    Actualizacion de cliente para que coincida con los cambios al backend

--- global/global.gd
+++ global/global.gd
@@ -2,4 +2,4 @@ extends Node
 var score: int = 0
 var username: String = "godot_user2"
 # cambiar por endpoint
-const api_url: String = "https://evsurofrnep5ivn34qaege2y3a0ykiqa.lambda-url.us-east-1.on.aws/"
+const api_url: String = "https://yadayadayada.lambda-url.us-east-1.on.aws/"
