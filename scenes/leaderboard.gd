db29ef4502d01bb9dc2e5871b7778e2d02444a32
German Soto <83991114+an3wworldfool@users.noreply.github.com>
2024-05-10 20:40:05 -0700

    Update leaderboard.gd

--- scenes/leaderboard.gd
+++ scenes/leaderboard.gd
@@ -3,23 +3,20 @@ extends Control
 @onready var actual_score_tree = $LeaderboardVContainer/ActualScoreTree
 @onready var record_label = $LeaderboardVContainer/RecordLabel
 var new_record = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Username: "+Global.username+" Score: "+str(Global.score))
-	$HTTPRequest.request_completed.connect(_on_request_completed)
+	print("hola")
 	var json = JSON.stringify({"username": Global.username, "high_score": Global.score})
 	var headers = ["Content-Type: application/json"]
 	$HTTPRequest.request(Global.api_url, headers, HTTPClient.METHOD_POST, json)
 
-func _on_request_completed(result, response_code, headers, body) -> void:
+
+func _on_http_request_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	#print(json)
+	print(json)
 	new_record = json["new_record"]
 	fill_leaderboard(json["Items"])
-
-
-func _on_close_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func fill_leaderboard(records_arr: Array) -> void:
 	var ast_root = actual_score_tree.create_item()
@@ -31,26 +28,29 @@ func fill_leaderboard(records_arr: Array) -> void:
 	root.set_text(1, "Username")
 	root.set_text(2, "Score")
 	root.set_text(3, "Scored On")
+	
 	var sorted_arr = ranking_sorting(records_arr)
 	for i in range(len(records_arr)):
 		var row = leaderboard.create_item(root)
 		row.set_text(0, str(i+1))
-		row.set_text(1, sorted_arr[i]["username"])
-		row.set_text(2, str(sorted_arr[i]["high_score"]))
-		row.set_text(3, sorted_arr[i]["score_date"])
+		row.set_text(1, sorted_arr[i]["username"]["S"])
+		row.set_text(2, str(sorted_arr[i]["high_score"]["N"]))
+		row.set_text(3, sorted_arr[i]["score_date"]["S"])
 	if(new_record):
 		record_label.text = "[img={48}x{48}]res://assets/crown24.png[/img][color=yellow]  [b]NEW RECORD!!![/b][/color]"
 	else:
 		record_label.text = "  [color=yellow]  [b]Current Score[/b][/color]"
-		
-
+	
 #funion para acomodar por fecha ascendente 
 func ranking_sorting(my_arr: Array) -> Array:
 	for i in range(my_arr.size()):
 		if(i+1 < my_arr.size()):
 			var a = my_arr[i]
 			var b = my_arr[i+1]
-			if(a["high_score"] == b["high_score"] and a["score_date"].naturalnocasecmp_to(b["score_date"])>0):
+			if(a["high_score"]["N"] == b["high_score"]["N"] and a["score_date"]["S"].naturalnocasecmp_to(b["score_date"]["S"])>0):
 				my_arr[i+1] = a
 				my_arr[i] = b
-	return my_arr
+	return my_arr	
+
+func _on_close_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
